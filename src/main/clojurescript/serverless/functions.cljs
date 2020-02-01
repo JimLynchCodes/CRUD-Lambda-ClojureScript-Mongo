4801210917c81680b0dcf9fb84a015471c52173b
JimTheMan <mrdotjim@gmail.com>
2020-02-01 10:47:44 -0500

    promesa attempt

--- src/main/clojurescript/serverless/functions.cljs
+++ src/main/clojurescript/serverless/functions.cljs
@@ -1,48 +1,50 @@
 (ns serverless.functions
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljs.nodejs :as nodejs]
-            [cljs.core.async :refer [<!]]))
+            [cljs.core.async :refer [<!]]
+            [promesa.core :as p]))
+
 
 (nodejs/enable-util-print!)
 (defonce moment (nodejs/require "moment"))
 (defonce mongoose (nodejs/require "mongoose"))
 
 (defn hello [_event ctx cb]
-(println ctx)
-(cb nil (clj->js
-{:statusCode 200
-:headers    {"Content-Type" "text/html"}
-:body       "<h1>Hello, World!</h1>"})))
+  (println ctx)
+  (cb nil (clj->js
+           {:statusCode 200
+            :headers    {"Content-Type" "text/html"}
+            :body       "<h1>Hello, World!</h1>"})))
 
 
 (defn now [_event ctx cb]
-(println ctx)
-(cb nil (clj->js
-{:statusCode 200
-:headers    {"Content-Type" "text/html"}
-:body       (str "<h1>" (.format (moment.) "LLLL") "</h1>")}))) ; call nodejs package
+  (println ctx)
+  (cb nil (clj->js
+           {:statusCode 200
+            :headers    {"Content-Type" "text/html"}
+            :body       (str "<h1>" (.format (moment.) "LLLL") "</h1>")}))) ; call nodejs package
 
 (defn example [_event _ctx cb]
-(println "happening! example")
-  
-  (go (let [connection (<! (. mongoose connect "mongodb://127.0.0.1:27017/local"))]
+  (println "happening! example")
+  (let [connection (. mongoose connect "mongodb://127.0.0.1:27017/local")
+        thenny (p/thenable? connection)]
+    (prn (str " thenable? " thenny))
+    (prn (str "connection is ", connection))
+
+  ; Trying to use core.async channels...
+  ; (go (let [connection (<! (. mongoose connect "mongodb://127.0.0.1:27017/local"))]
             ; the-glorious-data  (->> response
                                     ; (:body)
                                     ; (clj->js)
                                     ; (.stringify js/JSON))]
-        
-        
-        (prn (str "connection is ", connection))
-        
-        (cb nil (clj->js
-                 {:statusCode 200
-                  :headers    {"Content-Type" "application/json"}
-                  :body       "ok"}))))
-  )
+
+    (cb nil (clj->js
+             {:statusCode 200
+              :headers    {"Content-Type" "application/json"}
+              :body       "ok"}))))
 ; )
 
 (set! (.-exports js/module) #js
                              {:hello hello
                               :now now
-                              :example example
-                              })
+                              :example example})
